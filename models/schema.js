1a07778b81fd527305128829d4b609fe86bc2981
WalidBoussaid <walidmeknessi@hotmail.com>
2021-12-09 18:01:19 +0100

    database

---
+++ models/schema.js
@@ -0,0 +1,30 @@
+const Equipment = require("./equipment");
+// const Invoice = require("./invoice");
+const Use = require("./use");
+const User = require("./user");
+// const Role = require("./role");
+
+// Equipment.hasMany(Use);
+// Use.belongsTo(Equipment);
+
+// User.hasMany(Use);
+// Use.belongsTo(User);
+
+// Role.belongsToMany(User ,{
+//   through: "user_roles", 
+//   foreignKey: "roleId",
+// otherKey: "userId"});
+// User.belongsToMany(Role,{
+//   through: "user_roles", 
+// foreignKey: "userId",
+// otherKey: "roleId"})
+
+
+
+module.exports = {
+    Equipment,
+//   Invoice,
+    Use,
+    User,
+//   Role,
+};
