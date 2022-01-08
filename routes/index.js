9851a0b0d7ef44144ff451dc869c25c61151d014
WalidBoussaid <walidmeknessi@hotmail.com>
2022-01-08 17:27:43 +0100

    correction de supression du cookie apres un logout

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,8 @@ const {newFacture, getUserById, updateAdmin, createEquipement, updateEquipement,
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("login")
-  res.clearCookie('id');
   res.clearCookie('jwt_token');
+  res.render("login")
 });
 router.post("/login", async function (req, res){
 
